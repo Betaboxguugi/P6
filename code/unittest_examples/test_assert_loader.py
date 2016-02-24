2a8253cf9c72bf7d31c29160dcfae10a9ea1ae16
TheVikingGent <alexander.brandborg@hotmail.com>
2016-02-24 18:38:20 +0100

    SCD Type1 example
    Arbejder med SCD type1
    Ændrede også "code" mappen til scripts", da "code" er et reserveret
    navn.

--- code/unittest_examples/test_assert_loader.py
+++
@@ -1,7 +0,0 @@
-import unittest
-from code.unittest_examples.tests.test_asserts import TestAsserts
-
-#suite = unittest.defaultTestLoader.loadTestsFromModule(TestAsserts)
-#suite = unittest.makeSuite(TestAsserts)
-#unittest.TextTestRunner(verbosity=2).run(suite)
-
