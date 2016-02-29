e93df5c83c71ecbe36cd5bbc840c284bf3597856
Arash Michael Sami Kjær <arashkjr@gmail.com>
2016-02-29 15:27:43 +0100

    /RAGEQUIT

--- code/pygrametl_examples/example_1/fox_db.py
+++ code/pygrametl_examples/example_1/fox_db.py
@@ -8,25 +8,16 @@ import sys
 import os.path
 import pygrametl
 from pygrametl.datasources import SQLSource
-from pygrametl.tables import Dimension, FactTable
+from pygrametl.tables import Dimension, FactTable, SnowflakedDimension
 from pygrametl.steps import *
 import sqlite3
 from step_funcs import *
-
-# Insuring fresh database to work with by deleting old one
-if os.path.isfile('test.db'):
-   os.remove('test.db')
-   print("Deleted previous database")
-   conn = sqlite3.connect('test.db')
-else:
-   conn = sqlite3.connect('test.db')
-
-c = conn.cursor()
 
 # CONSTANTS
 
 # Going to try and make this.
 # http://www.dataonfocus.com/wp-content/uploads/2015/04/snowflake-schema-example.png
+
 
 def setup_in_db(db_name='./db.db'):
 
@@ -48,18 +39,19 @@ def setup_in_db(db_name='./db.db'):
                             position_name TEXT,
                             salary INT,
                             PRIMARY KEY (sale_ID)
-                            """)
+                            )""")
     sale_list = [(100, 'usd', 'here', 'fakestreet', 'jim', 35, '+1 456 798 435', 'lawyer', 10),
                  (200, 'dkk', 'der', 'iranvej', 'lars', 41, '+45 23 23 23 23', 'ehvervsmand', 50),
                  (230, 'eur', 'there', 'roma', 'pepe', 22, '+39 56 87 34 54', 'rare', 5),
                  (9000, 'yen', 'far', 'battlecruiser', 'yamato', 112, '+81 345 354 894', 'admiral', 5000000)]
 
-    db_cur.executemany("INSERT INTO sales(price, currency, location, address, name, age, contact, position_name, " +
-                       "description, salary) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", sale_list)
+    db_cur.executemany("INSERT INTO sales(price, currency, location, address, worker_name, age, contact, " +
+                       "position_name, salary) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?)", sale_list)
 
     db_conn.commit()
     db_conn.close()
     print("Operation done successfully")
+
 
 def setup_out_dw(dw_name='./dw.db'):
     if os.path.exists(dw_name):
@@ -98,9 +90,6 @@ def setup_out_dw(dw_name='./dw.db'):
                             name TEXT,
                             salary INT
                             );
-
-
-
                             """)
 
     dw_conn.commit()
@@ -111,17 +100,17 @@ DB_NAME = './db.db'
 DW_NAME = './dw.db'
 setup_in_db(DB_NAME)
 setup_out_dw(DW_NAME)
-db_conn = sqlite3.connect(DB_NAME)
-dw_conn = sqlite3.connect(DW_NAME)
-dw_conn_wrapper = pygrametl.ConnectionWrapper(connection=dw_conn)
+db_conn1 = sqlite3.connect(DB_NAME)
+dw_conn1 = sqlite3.connect(DW_NAME)
+dw_conn_wrapper = pygrametl.ConnectionWrapper(connection=dw_conn1)
 NAME_MAPPING = ("sale_ID, price, currency, location, address, name, age, contact, position_name, " +
-                "description, salary")
-sales_source = SQLSource(connection=db_conn, query="SELECT * FROM sales", names=NAME_MAPPING)
+                "salary")
+sales_source = SQLSource(connection=db_conn1, query="SELECT * FROM sales", names=NAME_MAPPING)
 
-fact_table = FactTable(
-    name='factTable',
-    keyrefs=['sale_ID', 'office_ID', 'sales_person_ID'],
-    measures=['sale'])
+sale_dimension = Dimension(
+    name='saleDim',
+    key='sale_ID',
+    attributes=['price', 'currency'])
 
 office_dimension = Dimension(
     name='officeDim',
@@ -131,34 +120,17 @@ office_dimension = Dimension(
 worker_dimension = Dimension(
     name='workerDim',
     key='sales_person_ID',
-    keyrefs='position_ID',
+    attributes=['name', 'age', 'contact'])
 
+position_dimension = Dimension(
+    name='positionDim',
+    key='position_ID',
+    attributes=['position_name', 'salary'])
 
-                            sales_person_ID INT,
-                            name TEXT,
-                            age INT,
-                            contact TEXT,
-                            position_ID,
-)
+special_snowflake = SnowflakedDimension(references=[(sale_dimension, office_dimension),
+                                                    (sale_dimension, worker_dimension),
+                                                    (worker_dimension, position_dimension)])
 
-# We create and object for each dimension in the DW and the FactTable
-book_dimension = Dimension(
-    name='bookDim',
-    key='bookid',
-    attributes=['book', 'genre'])
-
-time_dimension = Dimension(
-    name='timeDim',
-    key='timeid',
-    attributes=['day', 'month', 'year'])
-
-location_dimension = Dimension(
-    name='locationDim',
-    key='locationid',
-    attributes=['city', 'region'],
-    lookupatts=['city'])
-
-fact_table = FactTable(
-    name='factTable',
-    keyrefs=['bookid', 'locationid', 'timeid'],
-    measures=['sale'])
+dw_conn1.commit()
+dw_conn1.close()
+print("Operation done successfully")
