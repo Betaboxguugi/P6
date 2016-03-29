4a44f3f07d3e6d4057fde3207bb15eacb0627252
Mathias Jensen <betaboxguugi@gmail.com>
2016-03-29 09:07:43 +0200

    pygrametl reinterpreter

---
+++ code/pygrametl_reinterpreter/reinterpreter.py
@@ -0,0 +1,74 @@
+import ast
+from transform_visitor import TransformVisitor
+from extract_visitor import ExtractVisitor
+
+__author__ = 'Mathias Claus Jensen'
+__all__ = ['Reinterpreter']
+
+
+class Reinterpreter(object):
+    """ Class in charge of reinterpreting a pygrametl program, using different
+    connections.
+    """
+    def __init__(self, program, conn_scope, program_is_path=False):
+        """ 
+        :param program: A string containing the program that is to be 
+        reinterpreted or a path to it.
+        :conn_scope: A dictionary of string:connection pairs. Used to specify
+        which connections should be used in the program. The dictionary must be
+        ordered in the occurence of use in the program, and there has to be as
+        many connections in the dictionary as there are used in the program
+        :param program_is_path: Boolean that specifies if the program string is 
+        the actual program or a path to a file containing the program.
+        """
+        self.conn_scope = conn_scope
+        self.program = program
+        self.program_is_path = program_is_path
+        self.varname = 'extract_src_dict'
+
+        
+    def __transform(self, node):
+        """ Swaps out the connections in the old program, with the ones given.
+        """
+        tv = TransformVisitor(self.conn_scope)
+        tv.start(node)
+
+        
+    def __extract(self, node):
+        """ Makes and extracts DataSource objects for each dimension/facttable
+        in the program.
+        :return: An AST node that represents a dictionary containing all the 
+        dimension/facttable datasources. 
+        """
+        ev = ExtractVisitor(self.varname)
+        return ev.start(node)
+
+    
+    def __compile_exec(self, node, gscope=None, lscope=None):
+        """ Compiles and executes an AST node
+        """
+        p = compile(source=node, filename='<string>', mode='exec')
+        exec(p, gscope, lscope)
+
+        
+    def run(self):
+        """ Reinterpretes the pygrametl program, returns a dict containing 
+        :return: A dictionary with all the dimension/facttable datasources.
+        """
+        scope = self.conn_scope.copy()
+        program = ''
+        if self.program_is_path:
+            with open(self.program, 'r') as f:
+                program = f.read()
+        else:
+            program = self.program
+            
+        tree = ast.parse(program)
+        
+        self.__transform(tree)
+        self.__compile_exec(node=tree, gscope=None, lscope=scope)
+
+        src_module = self.__extract(tree)
+        self.__compile_exec(node=src_module, gscope=None, lscope=scope)
+        
+        return scope[self.varname]
---
+++ code/pygrametl_reinterpreter/transform_visitor.py
@@ -0,0 +1,78 @@
+import ast
+
+__author__ = 'Mathias Claus Jensen'
+__maintainer__ = 'Mathias Claus Jensen'
+__all__ = ['TransformVisitor']
+
+ATOMIC_SOURCES = ['SQLSource', 'CSVSource']
+AGGREGATED_SOURCES = ['JoiningSource']
+WRAPPERS = ['ConnectionWrapper']
+DIM_CLASSES = ['Dimension']
+FT_CLASSES = ['FactTable']
+MODIFY_LIST = ATOMIC_SOURCES + WRAPPERS
+
+class TransformVisitor(ast.NodeVisitor):
+    """
+    """
+    def __init__(self, conn_scope):
+        """
+        """
+        self.conn_scope = conn_scope
+        self._counter = 0
+
+        
+    def __get_id(self):
+        """ Iterates through the keys of the conn_dict.
+        """
+        key_list = [key for key in self.conn_scope.keys()]
+        id = key_list[self._counter]
+        if self._counter == len(key_list):
+            raise StopIteration('There are no more mappings to use')
+        else:
+            self._counter += 1
+        return id
+        
+        
+    def __replace_connection(self, node):
+        """ Replaces a connection in the given node with use specified ones.
+        :param node: The node for which a connection will be replaced.
+        """
+        id = self.__get_id()
+        newode = ast.Name(id=id, ctx=ast.Load())
+        if len(node.args) != 0:    # Conn given as positional arg
+            node.args[0] = newode
+        else:                      # Conn given by keyword
+            for keyword in node.keywords:
+                if keyword.arg == 'connection':
+                    keyword.value = newode
+        ast.fix_missing_locations(node)   
+        
+        
+    def __find_call_name(self, node):
+        """ Function that finds the name of a call node
+        :param node: The call node, whoms name we will find.
+        :return: The name of the call node
+        """
+        name = None
+        if hasattr(node.func, 'id'):       # SQLSource() type call
+            name = node.func.id
+        elif hasattr(node.func, 'attr'): # pygrametl.SQLSource() type call
+            name = node.func.attr
+        else:
+            raise NotImplementedError('Cannot get the name of ' + str(node))
+        return name
+
+        
+    def visit_Call(self, node):
+        """
+        """
+        name = self.__find_call_name(node)
+        if name in MODIFY_LIST:
+            self.__replace_connection(node)
+
+        
+    def start(self, node):
+        """
+        """
+        self._counter = 0
+        self.visit(node)
