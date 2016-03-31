af2ef4d3f8e57d8a6f1241c25c1276c398bb4218
Mathias Jensen <betaboxguugi@gmail.com>
2016-03-31 09:14:01 +0200

    example of a test program

--- code/pygrametl_reinterpreter/tests/brand_example.py
+++ code/pygrametl_reinterpreter/tests/brand_example.py
@@ -4,10 +4,13 @@ sys.path.append('../')
 from reinterpreter import Reinterpreter
 from db_test_setup import *
 import sqlite3
+import os.path
 
-DB = './brandtest.db'
-DW = './dwtest.db'
-CSV = './weapontest.csv'
+BASE_DIR = os.path.dirname(os.path.abspath('__file__'))
+DB = os.path.join(BASE_DIR, 'brandtest.db')
+DW = os.path.join(BASE_DIR, 'dwtest.db')
+CSV = os.path.join(BASE_DIR, 'weapontest.csv')
+
 
 program_path = './brand_sample.py'
 
@@ -21,7 +24,7 @@ dw_conn = sqlite3.connect(DW)
 
 #csv_conn = sqlite3.connect(CSV)
 
-conn_dict = {'conn0': dw_conn, 'conn1': db_conn}
+conn_dict = {'conn0': db_conn, 'conn1': dw_conn}
 
 try:
     tc = Reinterpreter(program=program_path, conn_scope=conn_dict, program_is_path=True)
@@ -29,3 +32,4 @@ try:
 finally:
     db_conn.close()
     dw_conn.close()
+    pass
