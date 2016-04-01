c9455090942d1ad7898c7987807e880a3a8508c1
Arash Michael Sami Kjær <arashkjr@gmail.com>
2016-04-01 11:46:10 +0200

    Bootiful integrity

--- code/test_predicates/referential_integrity_predicate.py
+++ code/test_predicates/referential_integrity_predicate.py
@@ -6,6 +6,11 @@ class ReferentialPredicate(TPredicate):
     def __init__(self, conn, referring_table_name, referred_table_name, key):
         """
         :param conn: you know by now
+        :param referring_table_name: Name of the table we are checking for referential integrity, the predicate is false
+        if there exists one key without a reference in the other table.
+        :param referred_table_name: Name of the table that should contain references to the keys in the former table,
+        the method also tests the 'reverse' integrity of this table
+        :param key: name of the key or ID that is tested for referential integrity
         """
         self.warehouse = self.dictify(conn)
         self.missing_keys = ()
@@ -14,16 +19,22 @@ class ReferentialPredicate(TPredicate):
         self.key = key
         self.referring_table = self.warehouse.get(referring_table_name)
         self.referred_table = self.warehouse.get(referred_table_name)
+        self.temp = None
+        self.temp_name = None
 
     def run(self):
-        """
-        :param referring_table_name: Name of the table we are checking for referential integrity, the predicate is false
-        if there exists one key without a reference in the other table.
-        :param referred_table_name: Name of the table that should contain references to the keys in the former table,
-        the method does not test referential integrity of this table. Recommend calling run() again with the table names
-        reversed.
-        :param key: name of the key or ID that is tested for referential integrity
-        """
+        self.ref_check()
+        self.ref_check()
+
+    def swap_tables(self):
+        self.temp = self.referring_table
+        self.referring_table = self.referred_table
+        self.referred_table = self.temp
+        self.temp_name = self.referring_table_name
+        self.referring_table_name = self.referred_table_name
+        self.referred_table_name = self.temp_name
+
+    def ref_check(self):
         self.missing_keys = ()
         self.__result__ = True
         for referring_row in self.referring_table:
@@ -38,6 +49,7 @@ class ReferentialPredicate(TPredicate):
                 self.missing_keys += referring_row,
                 self.__result__ = False
         self.report()
+        self.swap_tables()
 
     def report(self):
         if not self.__result__:
