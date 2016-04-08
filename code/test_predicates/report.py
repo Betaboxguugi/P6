eeb0760e7966d9eb9cd5f3e71773b7a38a427f42
Mikael Vind Mikkelsen <mvmi12@student.aau.dk>
2016-04-08 11:43:03 +0200

    ¯\_(ツ)_/¯
    Mindre ændring af report og not_null samt alle comments til report

--- code/test_predicates/report.py
+++ code/test_predicates/report.py
@@ -9,14 +9,19 @@ from pygrametl_reinterpreter import *
 
 
 class Report(object):
-    def __init__(self, name_of_predicate='', result=False, message_if_true=None, message_if_false=None,
-                 list_of_wrong_elements=()):
+    def __init__(self, name_of_predicate='', result=False, message_if_true='', message_if_false='',
+                 list_of_wrong_elements=None):
         """
-        :param name_of_predicate:
-        :param result:
-        :param message_if_true:
-        :param message_if_false:
-        :param list_of_wrong_elements:
+        :param name_of_predicate: name of the the predicate class used, for the lazy just use: self.__class__.__name__
+        :type name_of_predicate: str
+        :param result: final result of the test
+        :type result: bool
+        :param message_if_true: message which will be printed if results are True
+        :type message_if_false: str
+        :param message_if_false: message which will be printed if results are False
+        :type message_if_false: str
+        :param list_of_wrong_elements: list of all the elements which the predicate returned false to
+        :type list_of_wrong_elements: list
         """
         self.nop = name_of_predicate
         self.r = result
@@ -27,19 +32,17 @@ class Report(object):
 
     def run(self):
         """
-        Checks if results are true or false, 
+        Checks if results are true or false, prints the predicate used, result, message which is different dependent on
+        results and if result is false, also prints a list of the elements which returned false if any are provided.
+        In the cause that result is somehow neither, a failed message will show.
         """
         if self.r is True:
             print('{} returned {} {}'.format(self.nop, self.r, self.mit))
         elif self.r is False:
-            print('{} returned {} at the following elements {} {} '.format(self.nop, self.r, self.l, self.mif))
+            if self.l is not None:
+                print('{} returned {} at the following elements {} {} '.format(self.nop, self.r, self.l, self.mif))
+            else:
+                print('{} returned {} {} '.format(self.nop, self.r, self.mif))
         else:
-            print('Failure to report, please contact us at voresfællesemail@student.aau.dk if you see this message')
-
-
-"""
-Navn
-Boolean værdi
-evtuel besked
-information om entries der fejlede
-"""
+            # TODO: Make/get a mail people can report errors to
+            print('Failure to report, please contact us at errorReport@pyrgrametl.dk if you see this message')
