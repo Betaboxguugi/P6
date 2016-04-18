e0a524921b2af32494b13fd14acba8d9e142fc67
Arash Michael Sami Kjær <arashkjr@gmail.com>
2016-04-18 14:18:02 +0200

    Row number test code works agen. :kissing_closed_eyes:

--- code/framework/predicates/tests/row_test_code.py
+++ code/framework/predicates/tests/row_test_code.py
@@ -1,22 +1,22 @@
-import sys
-sys.path.append('../')
-sys.path.append('../../')
-import sqlite3
-from row_number_predicate import RowPredicate
-from datwarehouse_representation import DWRepresentation, DimRepresentation, FTRepresentation
+__author__ = 'Arash Michael Sami Kjær'
+__maintainer__ = 'Arash Michael Sami Kjær'
 
+import sqlite3
+from framework.predicates.row_number_predicate import RowPredicate
+from framework.datawarehouse_representation import DWRepresentation, DimRepresentation, FTRepresentation
 
 dw_name = '.\dw.db'  # The one found in pygrametl_examples
 dw_conn = sqlite3.connect(dw_name)
 
-f =  FTRepresentation('factTable', ['bookid','locationid','timeid'],['sale'], dw_conn)
-b = DimRepresentation('bookDim', 'bookid', ['genre'], ['book'], dw_conn)
-l = DimRepresentation('locationDim', 'locationid', ['region'], ['city'], dw_conn)
-t = DimRepresentation('timeDim', 'timeid', ['day', 'month', 'year'], [], dw_conn)
-Big = DWRepresentation([b,l,t],[f],dw_conn)
+book_dim = DimRepresentation('bookDim', 'bookid', ['book', 'genre'], dw_conn)
+time_dim = DimRepresentation('timeDim', 'timeid', ['day', 'month', 'year'], dw_conn)
+location_dim = DimRepresentation('locationDim', 'locationid', ['city', 'region'], dw_conn, ['city'])
+facttable = FTRepresentation('factTable', ['bookid', 'locationid', 'timeid'], ['sale'], dw_conn)
+dw = DWRepresentation([book_dim, time_dim, location_dim], [facttable], dw_conn)
 
-RowTest = RowPredicate(Big, 'factTable', 4)
+RowTest = RowPredicate('factTable', 4)
 
-RowTest.run()
-RowTest.report()
+RowTest.run(dw)
+report = RowTest.report()
+report.run()
 
