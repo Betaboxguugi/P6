e75cc5937d93d70ae5f30fd650b6f212f2bb62ef
TheVikingGent <alexander.brandborg@hotmail.com>
2016-05-05 16:03:29 +0200

    Functional dependency for multiple alpha and beta values.

--- code/framework/predicates/functional_dependency_predicate.py
+++ code/framework/predicates/functional_dependency_predicate.py
@@ -4,49 +4,56 @@ from .predicate import Predicate
 from .report import Report
 
 
-class FunctionalDependencyPredicate(Predicate): # TODO: Make this shit handle nulls
+class FunctionalDependencyPredicate(Predicate):
     """ Predicate that can check if a table or a join of tables holds certain
     functional dependencies.
     """
     
-    def __init__(self, tables, func_dependencies, ignore_None=False):
+    def __init__(self, tables, func_dependencies, ignore_none=False):
         """
         :param tables: tables from the database, which we wish to join
-        :param func_dependencies: functional dependencies between attributes, given
-        as a list of tubles. E.g. [('a', 'b'), ('a', 'c')], i.e. a -> b and a -> c
-        :ignore_none: Boolean, if true then we dont care what None values point at.
+        :param func_dependencies: functional dependencies between attributes,
+        given as a list of tuples.
+         E.g. [(('a'), ('b')), (('a','b'), ('c'))], i.e. a -> b and a,b -> c
+        :ignore_none: Boolean, if true then we don't care
+         what None values point at.
         """
 
-        self.cursor = None
         self.tables = tables
         self.func_dependencies = func_dependencies
         self.results = []
+        self.ignore_none = ignore_none
 
     def run(self, dw_rep):
         """
+        Checks whether each function dependency holds.
+        Uses a dictionary as a hash table. For each functional dependency,
+        it keeps of pairings of alpha and beta values. If we find several
+        beta values for one alpha, we register an error.
         """
-        vd = [{} for fd in self.func_dependencies]
+        hts = [{} for fd in self.func_dependencies]
         elements = []
 
         for row in dw_rep.iter_join(self.tables): # Natural join of tables
             for idx, fd in enumerate(self.func_dependencies):
-                x = row[fd[0]] 
-                y = row[fd[1]]
-                
-                if self.ignore_None and x == None:
+                alpha = (row[x] for x in fd[0])
+                beta = (row[y] for y in fd[1])
+
+                if self.ignore_none and any(x is None for x in alpha):
                     pass
-                elif x in vd[idx] and vd[idx][x] != y: # If the FD doesn't hold
+                # If the FD doesn't hold
+                elif alpha in hts[idx] and hts[idx][alpha] != beta:
                     elements.append((fd, row))
-                elif x in vd[idx]: # If we've seen this value before
-                    pass
-                else:
-                    vd[idx][x] = y # If we haven't
+                # If we haven't met alpha before
+                elif alpha not in hts[idx]:
+                    hts[idx][alpha] = beta
 
         result = not elements
         return Report(result=result,
                       tables=self.tables,
                       predicate=self,
                       elements=elements,
-                      msg='The predicate failed for the following rows, given as (predicate, row) tubles')
+                      msg='The predicate failed for the following rows,'
+                          ' given as (predicate, row) tuples')
                         
 
