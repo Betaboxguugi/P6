568756497b41a1669e52ad3a79398287a43781bc
Mikael Vind Mikkelsen <mvmi12@student.aau.dk>
2016-05-10 11:35:47 +0200

    setup_columns smidt op i predicate super classen

--- code/framework/predicates/no_duplicate_row_predicate.py
+++ code/framework/predicates/no_duplicate_row_predicate.py
@@ -40,26 +40,6 @@ class NoDuplicateRowPredicate(Predicate):
 
         return self.report()
 
-    def setup_columns(self, dw_rep):
-        # setup of columns, if column_names_exclude is true, then columns is
-        # all other columns than the one(s) specified.
-        if not self.column_names and not self.column_names_exclude:
-            self.column_names_exclude = True
-        # We can't iterate over a string so we convert self.column_names
-        # into a list if necessary.
-        if isinstance(self.column_names, str):
-            self.column_names = [self.column_names]
-        if self.column_names_exclude:
-            temp_columns_list = []
-            for e in dw_rep.get_data_representation(self.table_name).all:
-                temp_columns_list.append(e)
-            if self.column_names:
-                for e in self.column_names:
-                    temp_columns_list.remove(e)
-            self.columns = temp_columns_list
-        else:
-            self.columns = self.column_names
-
     def report(self):
 
         return Report(result=self.__result__,
--- code/framework/predicates/rule_column_predicate.py
+++ code/framework/predicates/rule_column_predicate.py
@@ -17,25 +17,6 @@ class RuleColumnPredicate(Predicate):
         self.constraint_input_list = constraint_input_list
         self.columns = []
 
-    def setup_columns(self, dw_rep):
-
-        # setup of columns, if column_names_exclude is true, then columns is
-        # all other columns than the one(s) specified.
-        if not self.column_names and not self.column_names_exclude:
-            self.column_names_exclude = True
-        # We can't iterate over a string so we convert self.column_names
-        # into a list if necessary.
-        if isinstance(self.column_names, str):
-            self.column_names = [self.column_names]
-        if self.column_names_exclude:
-            temp_columns_list = []
-            for column in dw_rep.get_data_representation(self.table_name).all:
-                temp_columns_list.append(column)
-            if self.column_names:
-                for column_name in self.column_names:
-                    temp_columns_list.remove(column_name)
-            self.column_names = temp_columns_list
-
     def run(self, dw_rep):
 
         self.columns = []
