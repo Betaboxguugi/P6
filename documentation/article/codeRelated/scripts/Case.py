f5be1c82df6635e3376f207cb487bac93fd792eb
Mathias Jensen <betaboxguugi@gmail.com>
2016-05-11 08:31:49 +0200

    overview

--- documentation/article/codeRelated/scripts/Case.py
+++ documentation/article/codeRelated/scripts/Case.py
@@ -1,7 +1,24 @@
-def Case(program = './program',
-         sources =  [source1,source2],
-         pred_list = [predicate1,predicate2],
-         pep249_module = sqlite3,
-         program_is_path=True,
-         database = './dw'
-         )
+import sqlite3
+
+# We execute a pygrametl program using our specified
+# sources and create a DWRepresentation.
+program = './myetlprogram.py'
+src1 = sqlite3.connect(datasource='./db1')
+src2 = sqlite3.connect(datasource='./db2')
+dwp = DWPopulator(program=program,
+                  pep249_module=sqlite3,
+                  sources=(src1, src2),
+                  datasource='./dw')
+
+dwrep = dwp.run()
+
+# We create our predicates
+p1 = ReferentialIntegretyPredicate()
+p2 = RowCountPredicate(table_name='table1',
+                       number_of_rows=99)
+
+# We create our Case and run it
+c = Case(dw_rep=dwrep,
+         pred_list=[p1, p2])
+c.run()
+
---
+++ documentation/article/codeRelated/scripts/ReferentialIntegretyPredicate.py
@@ -0,0 +1,1 @@
+ReferentialIntegrityPredicate() # Takes no arguments
