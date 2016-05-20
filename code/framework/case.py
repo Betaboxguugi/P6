694f7a053c995ce4653e955c3274952c8bed4d92
TheVikingGent <alexander.brandborg@hotmail.com>
2016-05-20 10:42:38 +0200

    Just looking over case and DWRep

--- code/framework/case.py
+++ code/framework/case.py
@@ -8,8 +8,7 @@ class Case:
         Executes predicates upon a DWRepresentation associated with a DW
         :param pred_list: list of predicates
         :param dw_rep: object representing a DW.
-        OBS! connection must not be closed! Maybe include connection parameters
-        in rep?
+        OBS! connection in dw_rep must not be closed!
         """
         self.pred_list = pred_list
         self.dw_rep = dw_rep
@@ -21,7 +20,7 @@ class Case:
             r = p.run(self.dw_rep)
             reports.append(r)
 
-        # For debugging purposes, replace with some CL GUI stuff, maybe :D
+        # Prints the report of each predicate
         for r in reports:
             if isinstance(r,list):
                 for subreport in r:
--- code/framework/datawarehouse_representation.py
+++ code/framework/datawarehouse_representation.py
@@ -1,53 +1,51 @@
 __author__ = 'Mathias Claus Jensen & Alexander Brandborg'
 __maintainer__ = 'Mathias Claus Jensen'
 
+
 class DWRepresentation(object):
     """
     Class used to represent an entire DW.
     Allows for access to specific tables simply through their name.
     """
 
-    def __init__(self, dims,  connection, fts=[], snowflakeddims=()):
+    def __init__(self, dims,  connection, fts=None, snowflakeddims=()):
         """
         :param dims: A list of DimensionRepresentation Objects
-        :param fts: A lost of FTRepresentation Objects
+        :param fts: A list of FTRepresentation Objects
         :param snowflakeddims: Tuple of SnowflakedDimensions
         :param connection: A PEP 249 connection to a database
         """
 
-        try:
-            self.dims = dims
+        self.dims = dims
+        if fts is None:
+            self.fts = []
+        else:
             self.fts = fts
-            self.connection = connection
-            self.snowflakeddims = snowflakeddims
+        self.connection = connection
+        self.snowflakeddims = snowflakeddims
 
-            # Turns all our names to lower case as SQL is case insensitive
-            # Also collects a list of names for a later check
-            name_list = []
-            self.rep = self.dims + self.fts
+        # Turns all our names to lower case as SQL is case insensitive
+        # Also collects a list of names for a later check
+        name_list = []
+        self.rep = self.dims + self.fts
 
-            for entry in self.rep:
-                low = entry.name.lower()
-                entry.name = low
-                name_list.append(low)
+        for entry in self.rep:
+            low = entry.name.lower()
+            entry.name = low
+            name_list.append(low)
 
-            # Makes sure that no two tables have the same name.
-            # Else we raise an exception.
-            if len(name_list) != len(list(set(name_list))):
-                raise ValueError("Table names are not unique")
+        # Makes sure that no two tables have the same name.
+        # Else we raise an exception.
+        if len(name_list) != len(list(set(name_list))):
+            raise ValueError("Table names are not unique")
 
-            # Fills the dictionary with tables keyed by their names.
-            self.tabledict = {}
-            for entry in self.rep:
-                self.tabledict[entry.name] = entry
+        # Fills the dictionary with tables keyed by their names.
+        self.tabledict = {}
+        for entry in self.rep:
+            self.tabledict[entry.name] = entry
 
-            # Re-creates the referencing structure of the DW
-            self.refs = self._find_structure()
-        finally:
-            try:
-                pass
-            except Exception:
-                pass
+        # Re-creates the referencing structure of the DW
+        self.refs = self._find_structure()
 
     def _find_structure(self):
         """
@@ -57,7 +55,7 @@ class DWRepresentation(object):
         and dimensions.
         For this to work there are some restrictions to keep in mind:
         - Facttable may only refer to the root of a Snowflaked Dimension.
-        - There may be no overlap between the dimensions of on Snowflaked
+        - There may be no overlap between the dimensions of a Snowflaked
           dimension and another.
         - Primary/Foreign key pairs have to share attribute name.
 
@@ -98,21 +96,36 @@ class DWRepresentation(object):
         return references
 
     def _find_dim_rep(self, dim, all_dims):
+        """
+        Used when  handling snowflaking.
+        Allows us to find the representation object corresponding to a name
+        :param dim: Name of dimension
+        :param all_dims: List of all representation objects
+        :return: The corresponding dimension representation object
+        """
+
         for rep in all_dims:
             if rep.name == dim.name:
                 return rep
         raise Exception('Snowflaked dimension rep not found.')
 
     def __str__(self):
+        """
+        :return: A string representation of the object
+        """
         return self.tabledict.__str__()
 
     def __repr__(self):
+        """
+        :return: A representation of the object
+        """
         return self.__str__()
 
     def iter_join(self, names):
-        """ Iterate over a natural join of the given table names
+        """
+        Iterate over a natural join of the given table names
         :param names: List of table names
-        :yield: A dictionary representing a row
+        :return: A generator for iterating over contents of a join
         """
         if isinstance(names, str):
             query = "SELECT * FROM " + names
@@ -125,10 +138,14 @@ class DWRepresentation(object):
 
             while True:
                 data = cursor.fetchmany(500)
+                # Some cursor.description return null if the cursor hasn't
+                # fetched.
+                # Thus we call it again after fetch if this is the case.
                 if not names:
                     names = [t[0] for t in cursor.description]
                 if not data:
                     break
+                # Checks that the entries have the correct amount of attributes
                 if len(names) != len(data[0]):
                     raise ValueError(
                         "Incorrect number of names provided. " +
@@ -150,7 +167,8 @@ class DWRepresentation(object):
 
 
 class TableRepresentation(object):
-    """ Super class for representing tables in a DW
+    """
+    Super class for representing tables in a DW
     """
 
     def __iter__(self):
@@ -185,7 +203,8 @@ class TableRepresentation(object):
                 pass
 
     def itercolumns(self, column_names):
-        """Lets us fetch only a subset of columns from the table
+        """
+        Lets us fetch only a subset of columns from the table
         :param column_names: The subset of columns of interest
         :return: A generator for iterating over the contents of the table
         """
@@ -198,14 +217,11 @@ class TableRepresentation(object):
 
 class DimRepresentation(TableRepresentation):
     """
-    An object for representing data in a DW dimension
+    A class for representing data in a DW dimension
     """
     def __init__(self, dimension, connection):
         """
-        :param name: Name of table
-        :param key: Name of primary key attribute
-        :param attributes: List of non-lookup attributes of the table
-        :param lookupatts: List of lookup attributes of the table
+        :param dimension: pygrametl dimension object
         :param connection: PEP249 connection to a database
         """
         self.name = dimension.name
@@ -217,22 +233,42 @@ class DimRepresentation(TableRepresentation):
         self.query = "SELECT " + ",".join(self.all) + " FROM " + self.name
 
     def __str__(self):
+        """
+        :return: A string representation of the object
+        """
         text = "{} {} {} {}".format(self.name, self.key, self.attributes,
                                     self.lookupatts)
         return text
 
     def __repr__(self):
+        """
+        :return: A representation of the object
+        """
         return self.__str__()
 
 
 class SCDType1DimRepresentation(DimRepresentation):
+    """
+    A class for representing data in a SCDType1 DW dimension
+    """
     def __init__(self, dimension, connection):
+        """
+        :param dimension: pygrametl dimension object
+        :param connection: PEP249 connection to a database
+        """
         DimRepresentation.__init__(self, dimension, connection)
         self.type1atts = dimension.type1atts
 
 
 class SCDType2DimRepresentation(DimRepresentation):
+    """
+    A class for representing data in a SCDType2 DW dimension
+    """
     def __init__(self, dimension, connection):
+        """
+        :param dimension: pygrametl dimension object
+        :param connection: PEP249 connection to a database
+        """
         DimRepresentation.__init__(self, dimension, connection)
         self.versionatt = dimension.versionatt
         self.fromatt = dimension.fromatt
@@ -244,11 +280,8 @@ class FTRepresentation(TableRepresentation):
     """
     def __init__(self, factable, connection):
         """
-        :param name: Name of table
-        :param keyrefs: List of attributes that are foreign keys to other
-        tables
+        :param factable: pygrametl facttable object
         :param connection: PEP249 connection to a database
-        :param measures: List of attributes containing non-key values
         """
         self.name = factable.name
         self.keyrefs = factable.keyrefs
@@ -261,8 +294,14 @@ class FTRepresentation(TableRepresentation):
         self.query = "SELECT " + ",".join(self.all) + " FROM " + self.name
 
     def __str__(self):
+        """
+        :return: A string representation of the object
+        """
         text = "{} {} {}".format(self.name, self.keyrefs, self.measures)
         return text
 
     def __repr__(self):
+        """
+        :return: A representation of the object
+        """
         return self.__str__()
