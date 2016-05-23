c2054edd8dcda6bc681a59bc4a9b593ab48d1504
Mikael Vind Mikkelsen <mvmi12@student.aau.dk>
2016-05-23 14:22:32 +0200

    SQL fix FunctinalDependency

--- documentation/article/codeRelated/scripts/SQLFunctionalDependency.sql
+++ documentation/article/codeRelated/scripts/SQLFunctionalDependency.sql
@@ -1,4 +1,4 @@
-SELECT t1.country, t1.city 
+SELECT DISTINCT t1.country, t1.city
 FROM countrydim NATURAL JOIN authordim AS t1, countrydim NATURAL JOIN authordim AS t2
 WHERE t1.city = t2.city 
 AND t1.country <> t2.country
