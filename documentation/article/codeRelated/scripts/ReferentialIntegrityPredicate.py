788ea71b204fe0a69eea1b76ab44b662d3ec6cac
Mathias Jensen <betaboxguugi@gmail.com>
2016-05-23 16:45:39 +0200

    fix ref_pred

--- documentation/article/codeRelated/scripts/ReferentialIntegrityPredicate.py
+++ documentation/article/codeRelated/scripts/ReferentialIntegrityPredicate.py
@@ -1,5 +1,5 @@
 ReferentialIntegrityPredicate(
-    refs={facttable: [BookDim, AuthorDim], AuthorDim: [CountryDim]},
+    refs={'FactTable': ('BookDim', 'AuthorDim'), 'AuthorDim': ('CountryDim')},
     points_to_all=True,
     all_pointed_to=True
 )
