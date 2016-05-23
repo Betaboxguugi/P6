f0103f029fea5372b96e6fb37bf401e020e06fc1
TheVikingGent <alexander.brandborg@hotmail.com>
2016-05-23 14:36:28 +0200

    PRED 3

--- documentation/article/codeRelated/scripts/FunctionalDependencyPredicate.py
+++ documentation/article/codeRelated/scripts/FunctionalDependencyPredicate.py
@@ -1,4 +1,4 @@
-FunctionalDependencyPredicate(tables=['CountryDim',
+FunctionalDependencyPredicate(table_name=['CountryDim',
                                       'AuthorDim'],
                               attributes='city',
                               dependent_attributes='country')
