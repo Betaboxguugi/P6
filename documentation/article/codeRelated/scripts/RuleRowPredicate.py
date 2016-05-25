075560478f95f3f0d185b702e57582fed987944b
Arash Michael Sami Kjær <arashkjr@gmail.com>
2016-05-25 11:12:03 +0200

    Autobios for helvede altså omg

--- documentation/article/codeRelated/scripts/RuleRowPredicate.py
+++ documentation/article/codeRelated/scripts/RuleRowPredicate.py
@@ -1,8 +1,8 @@
-def no_autos(name, title):
+def no_autobios(name, title):
     return not name == title 
 
 RuleRowPredicate(table_name=['AuthorDim','FactTable','BookDim']
-                 constraint_function=no_autos,
+                 constraint_function=no_autobios,
                  column_names=['name', 'title'],
                  constraint_args=[],
                  column_names_exclude=False)
