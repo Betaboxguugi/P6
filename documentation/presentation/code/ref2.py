4ca11c04110cc9b4a42943b8d5c5a6cfba322931
Mikael Vind Mikkelsen <mvmi12@student.aau.dk>
2016-06-17 06:48:55 +0200

    Let's do this! :smiley:

--- documentation/presentation/code/ref2.py
+++ documentation/presentation/code/ref2.py
@@ -1,9 +1,10 @@
-        else:
-            for x in beta:
-                if isinstance(x, str):
-                    b.append(dw_rep.
-                                 get_data_representation(x))
-                else:
-                    raise ValueError('Expected string' + ' in refs, got: ' + str(type(x)))
-        refs[a] = tuple(b)
-    self.refs = refs
+    if isinstance(beta, str):
+        b.append(dw_rep.get_data_representation(beta))
+            else:
+                for x in beta:
+                    if isinstance(x, str):
+                        b.append(dw_rep.get_data_representation(x))
+                    else:
+                        raise ValueError('Expected string' + ' in refs, got: ' + str(type(x)))
+            refs[a] = tuple(b)
+        self.refs = refs
